44644ebab0f715f09bc785c5db958f43acfdec87
kerleewong <k.lee_926@yahoo.com>
2019-04-02 23:03:28 +0800

    w06 assign1

---
+++ Assign1/js/p4.js
@@ -0,0 +1,35 @@
+
+
+
+function goPage(index) {
+    
+    var p = document.getElementById('content');
+        switch(index){
+        case 1 : document
+
+        p.innerHTML = '<iframe width="800" height = "600" src="p1/div.html" frameborder="0"></iframe>';
+       
+    
+        break;
+
+        case 2: document
+       
+       p.innerHTML = '<iframe width="800" height = "600" src="p2/tku60.html" frameborder="0"></iframe>';
+       
+       break;
+
+       case 3: document
+
+p.innerHTML = '<iframe width="800" height = "600" src="p3/mytravel.html" frameborder="0"></iframe>';
+       
+       break;
+    }
+}
+    
+    //var newHTML = 
+    //document.getElementById('content').innerHTML = newHTML;
+
+ 
+
+  //<iframe width="800" height = "600" src="p2/tku60.html" frameborder="0"></iframe>';
+    
---
+++ Assign1/p2/js/tku60.js
@@ -0,0 +1,31 @@
+function vid1(){
+
+    document.getElementById('vid1').src="https://www.youtube.com/embed/RechrtUxfQc"
+}
+
+
+function vid2() {
+
+document.getElementById('vid1').src="https://www.youtube.com/embed/MRWX49Furew"
+
+}
+
+function vid3(){
+
+    document.getElementById('vid1').src="https://www.youtube.com/embed/METhdbL_iMw"
+
+}
+
+function vid4(){
+
+    document.getElementById('vid1').src="https://www.youtube.com/embed/ZyDbq-lEKTo"
+
+}
+
+function vid5(){
+
+    document.getElementById('vid1').src="https://www.youtube.com/embed/NlsrJbVvjaA"
+
+}
+
+
